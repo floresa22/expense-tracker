b17fe757a038053bd0e9bb3a6f5474484881008a
Anthony Flores <anthony.flores2293@gmail.com>
2020-05-29 21:31:52 -0700

    final version complete

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 10000000),
+      text,
+      amount,
+    };
+
+    addTransaction(newTransaction);
+  };
+
   return (
     <>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" placeholder="Enter text..." />
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text..."
+          />
         </div>
         <div className="form-control">
           <label htmlFor="amount">
@@ -15,9 +37,16 @@ export const AddTransaction = () => {
             <br />
             (negative - expense, positive - income)
           </label>
-          <input type="number" placeholder="Enter amount..." />
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(JSON.parse(e.target.value))}
+            placeholder="Enter amount..."
+          />
         </div>
-        <button className="btn">Add Transaction</button>
+        <button onClick={onSubmit} className="btn">
+          Add Transaction
+        </button>
       </form>
     </>
   );
